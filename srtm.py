bf3f25ab2e277a3707aaaf9e44a7dd729e52aba6
stefanieqiu <stefanieqiu@hotmail.com>
2018-04-03 10:27:35 -0400

    Add files via upload
    made change for N,W and added bilinear interpolation

--- srtm.py
+++ srtm.py
@@ -1,11 +1,12 @@
 from __future__ import print_function
 import os
 import numpy as np
+import math
 
 SRTM_DICT = {'SRTM1': 3601, 'SRTM3': 1201}
 
-# Get the type of SRTM files or use SRTM3 by default
-SRTM_TYPE = os.getenv('SRTM_TYPE', 'SRTM3')
+# Get the type of SRTM files
+SRTM_TYPE = os.getenv('SRTM_TYPE', 'SRTM1')
 SAMPLES = SRTM_DICT[SRTM_TYPE]
 
 # put uncompressed hgt files in HGT_DIR, defaults to 'hgt'
@@ -19,8 +20,35 @@ def get_elevation(lat, lon):
     # Treat it as data void as in SRTM documentation
     # if file is absent
     return -32768
+def bilinear_interpolation(x, y, points):
+    '''Interpolate (x,y) from values associated with four points.
 
+    The four points are a list of four triplets:  (x, y, value).
+    The four points can be in any order.  They should form a rectangle.
 
+         bilinear_interpolation(12, 5.5,
+        ...                        [(10, 4, 100),
+        ...                         (20, 4, 200),
+        ...                         (10, 6, 150),
+        ...                         (20, 6, 300)])
+        165.0
+
+    '''
+    # See formula at:  http://en.wikipedia.org/wiki/Bilinear_interpolation
+
+    points = sorted(points)               # order points by x, then by y
+    (x1, y1, q11), (_x1, y2, q12), (x2, _y1, q21), (_x2, _y2, q22) = points
+
+    if x1 != _x1 or x2 != _x2 or y1 != _y1 or y2 != _y2:
+        raise ValueError('points do not form a rectangle')
+    if not x1 <= x <= x2 or not y1 <= y <= y2:
+        raise ValueError('(x, y) not within the rectangle')
+
+    return (q11 * (x2 - x) * (y2 - y) +
+            q21 * (x - x1) * (y2 - y) +
+            q12 * (x2 - x) * (y - y1) +
+            q22 * (x - x1) * (y - y1)
+           ) / ((x2 - x1) * (y2 - y1) + 0.0)
 def read_elevation_from_file(hgt_file, lat, lon):
     with open(hgt_file, 'rb') as hgt_data:
         # HGT is 16bit signed integer(i2) - big endian(>)
@@ -30,12 +58,26 @@ def read_elevation_from_file(hgt_file, lat, lon):
             SAMPLES * SAMPLES  # length
         ).reshape((SAMPLES, SAMPLES))
 
-        lat_row = int(round((lat - int(lat)) * (SAMPLES - 1), 0))
-        lon_row = int(round((lon - int(lon)) * (SAMPLES - 1), 0))
+        lat_row =round((lat - int(lat)) * (SAMPLES - 1),4)
+        lat_bottom = math.floor(lat_row)
+        if lat_row == lat_bottom:
+            lat_top = lat_row
+        else:
+            lat_top = math.ceil(lat_row)
+        lon_row = -round((lon - int(lon)) * (SAMPLES - 1),4)
+        lon_right = math.ceil(lon_row)
+        if lon_right == lon_row:
+            lon_left = lon_right
+        else:
+            lon_left = math.floor(lon_row)
+            lat_bottom = SAMPLES - 1 - lat_bottom
+        lat_top = SAMPLES - 1 - lat_top
+        ind_lat = lat_row - int(lat_row)
+        ind_lon = lon_row - int(lon_row)
+        n = [(0, 0, elevations[int(lat_top),int(lon_left)]),(1, 0, elevations[int(lat_bottom),int(lon_left)]),(0, 1, elevations[int(lat_top),int(lon_right)]),(1, 1, elevations[int(lat_bottom),int(lon_right)]),]
+        x = bilinear_interpolation(ind_lat, ind_lon, n)
 
-        return elevations[SAMPLES - 1 - lat_row, lon_row].astype(int)
-
-
+        return x
 def get_file_name(lat, lon):
     """
     Returns filename such as N27E086.hgt, concatenated
@@ -52,16 +94,13 @@ def get_file_name(lat, lon):
         ew = 'W'
 
     hgt_file = "%(ns)s%(lat)02d%(ew)s%(lon)03d.hgt" % \
-               {'lat': abs(lat), 'lon': abs(lon), 'ns': ns, 'ew': ew}
+               {'lat': abs(lat), 'lon': abs(lon)+1, 'ns': ns, 'ew': ew} # N,W
     hgt_file_path = os.path.join(HGTDIR, hgt_file)
     if os.path.isfile(hgt_file_path):
         return hgt_file_path
     else:
         return None
 
-
 if __name__ == '__main__':
-    # Mt. Everest
-    print('Mt. Everest: %d' % get_elevation(27.988056, 86.925278))
-    # Kanchanjunga
-    print('Kanchanjunga: %d' % get_elevation(27.7025, 88.146667))
+    print('Elevation: %f' % get_elevation(43.524, -85.7523))
+
